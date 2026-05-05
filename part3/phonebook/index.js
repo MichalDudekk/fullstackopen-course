af7f4c046163539ad14e14bdc2fb093c800398ed
Michal Dudek <michaldudek.contact@gmail.com>
2026-05-06 01:33:46 +0200

    Make phonebook app ready to deploy

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -114,7 +115,6 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
