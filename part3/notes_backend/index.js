1d8225dd884b0749242da90ccb5471afed6f4a00
Michal Dudek <michaldudek.contact@gmail.com>
2026-05-15 21:47:28 +0200

    Make get_note_by_id and post_note work on a real database

--- part3/notes_backend/index.js
+++ part3/notes_backend/index.js
@@ -41,16 +41,14 @@ app.get("/api/notes", (request, response) => {
 });
 
 app.get("/api/notes/:id", (request, response) => {
-    const id = request.params.id;
-    const note = notes.find((note) => note.id === id);
-    // response.send(`${typeof id} ${typeof note.id}`);
-
-    if (note) {
-        response.json(note);
-    } else {
-        response.statusMessage = `Note id = ${id} not found.`;
-        response.status(404).end();
-    }
+    Note.findById(request.params.id).then((note) => {
+        if (note) {
+            response.json(note);
+        } else {
+            response.statusMessage = `Note id = ${request.params.id} not found.`;
+            response.status(404).end();
+        }
+    });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -70,20 +68,17 @@ app.post("/api/notes", (request, response) => {
     const body = request.body;
 
     if (!body.content) {
-        return response.status(400).json({
-            error: "content missing",
-        });
+        return response.status(400).json({ error: "content missing" });
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        id: generateId(),
-    };
+    });
 
-    notes = notes.concat(note);
-
-    response.json(note);
+    note.save().then((savedNote) => {
+        response.json(savedNote);
+    });
 });
 
 app.listen(PORT, () => {
