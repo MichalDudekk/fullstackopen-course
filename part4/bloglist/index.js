33c22c5d5852967c6f20d8923ca95d8bb3008673
Michal Dudek <michaldudek.contact@gmail.com>
2026-07-01 15:13:22 +0200

    Make application bloglist work (Ad 4.1)

---
+++ part4/bloglist/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { PORT, MONGODB_URI } = require('./utils/config.js');
+
+const app = express();
+
+const blogSchema = mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number,
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+console.log(`connecting to: ${MONGODB_URI}`);
+mongoose
+    .connect(MONGODB_URI, { family: 4 })
+    .then(() => console.log('succesfully connected to MongoDB'))
+    .catch((e) => console.log(`error contecting to mongodb: ${e}`));
+
+app.use(express.json());
+
+app.get('/api/blogs', (request, response) => {
+    Blog.find({}).then((blogs) => {
+        response.json(blogs);
+    });
+});
+
+app.post('/api/blogs', (request, response) => {
+    const blog = new Blog(request.body);
+
+    blog.save().then((result) => {
+        response.status(201).json(result);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
