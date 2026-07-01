442001432138888d8e2ad49586ab1ef230d9a5c9
Michal Dudek <michaldudek.contact@gmail.com>
2026-07-01 15:29:27 +0200

    Add logger.js

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,15 +1,17 @@
+// index.js
 const express = require('express');
 const mongoose = require('mongoose');
 const { PORT, MONGODB_URI } = require('./utils/config.js');
 const Blog = require('./models/blog.js');
+const { info, error } = require('./utils/logger.js');
 
 const app = express();
 
-console.log(`connecting to: ${MONGODB_URI}`);
+info(`connecting to: ${MONGODB_URI}`);
 mongoose
     .connect(MONGODB_URI, { family: 4 })
-    .then(() => console.log('succesfully connected to MongoDB'))
-    .catch((e) => console.log(`error contecting to mongodb: ${e}`));
+    .then(() => info('succesfully connected to MongoDB'))
+    .catch((e) => error(`error contecting to mongodb: ${e}`));
 
 app.use(express.json());
 
@@ -28,5 +30,5 @@ app.post('/api/blogs', (request, response) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    info(`Server running on port ${PORT}`);
 });
