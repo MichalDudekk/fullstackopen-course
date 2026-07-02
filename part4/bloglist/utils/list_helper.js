832c6c3d9311968944429d5191d5bacc50a28374
Michal Dudek <michaldudek.contact@gmail.com>
2026-07-02 21:48:23 +0200

    Add tests for favoriteBlog (Ad 4.5*)

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -8,7 +8,22 @@ const totalLikes = (blogs) => {
     return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
 };
 
+const favoriteBlog = (blogs) => {
+    let maximum = 0;
+    let result = null;
+
+    for (const blog of blogs) {
+        if (blog.likes > maximum) {
+            maximum = blog.likes;
+            result = blog;
+        }
+    }
+
+    return result;
+};
+
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog,
 };
