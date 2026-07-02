dc67bbc2237eaca40ffe36e44831fd4f582fd2a9
Michal Dudek <michaldudek.contact@gmail.com>
2026-07-02 22:22:38 +0200

    Add mostBlogs function to list_helper.js and test for it (Ad 4.6*)

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -22,8 +22,30 @@ const favoriteBlog = (blogs) => {
     return result;
 };
 
+const mostBlogs = (blogs) => {
+    const authorBlogs = new Map();
+
+    for (const blog of blogs) {
+        const author = blog.author;
+        authorBlogs.set(author, (authorBlogs.get(author) || 0) + 1);
+    }
+
+    let maximumBlogs = 0;
+    let authorWithMostBlogs = null;
+
+    for (const [author, numberOfTheirBlogs] of authorBlogs) {
+        if (numberOfTheirBlogs > maximumBlogs) {
+            maximumBlogs = numberOfTheirBlogs;
+            authorWithMostBlogs = author;
+        }
+    }
+
+    return { author: authorWithMostBlogs, blogs: maximumBlogs };
+};
+
 module.exports = {
     dummy,
     totalLikes,
     favoriteBlog,
+    mostBlogs,
 };
