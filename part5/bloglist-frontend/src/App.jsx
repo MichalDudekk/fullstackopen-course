5111fba2c97ec6992fff70119eee699aed2d9816
Michal Dudek <michaldudek.contact@gmail.com>
2026-07-20 04:42:54 +0200

    Add frontend draft to blogsapp (Ad 5.1)

---
+++ part5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,78 @@
+import { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+import loginService from './services/login';
+
+const App = () => {
+    const [blogs, setBlogs] = useState([]);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        blogService.getAll().then((blogs) => setBlogs(blogs));
+    }, []);
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+
+        try {
+            const newUser = await loginService.login({ username, password });
+            setUser(newUser);
+            setUsername('');
+            setPassword('');
+
+            blogService.setToken(newUser.token);
+        } catch {
+            alert('Wrong credentials');
+        }
+    };
+
+    const loginForm = () => (
+        <>
+            <h2>Log in to application</h2>
+            <form onSubmit={handleLogin}>
+                <div>
+                    <label>
+                        username
+                        <input
+                            type="text"
+                            value={username}
+                            onChange={({ target }) => setUsername(target.value)}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        password
+                        <input
+                            type="text"
+                            value={password}
+                            onChange={({ target }) => setPassword(target.value)}
+                        />
+                    </label>
+                </div>
+                <button type="submit">login</button>
+            </form>
+        </>
+    );
+
+    const blogList = () => (
+        <div>
+            {`${user.name} is logged in`}
+            <h2>blogs</h2>
+            {blogs.map((blog) => (
+                <Blog key={blog.id} blog={blog} />
+            ))}
+        </div>
+    );
+
+    return (
+        <>
+            {!user && loginForm()}
+            {user && blogList()}
+        </>
+    );
+};
+
+export default App;
