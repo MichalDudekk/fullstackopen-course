b4e2352ab6ef2aca2b08aafe9793ef1ffb4ec70f
Michal Dudek <michaldudek.contact@gmail.com>
2026-07-28 19:29:43 +0200

    Add details to Blog component (Ad 5.7)

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5,
+    };
+
+    const [visible, setVisible] = useState(false);
+
+    const toggleVisibility = () => setVisible(!visible);
+
+    return (
+        <div style={blogStyle}>
+            <div>
+                {blog.title} {blog.author}
+                <button onClick={toggleVisibility}>
+                    {visible ? 'hide' : 'view'}
+                </button>
+            </div>
+            {visible && (
+                <div>
+                    {blog.url}
+                    <br />
+                    likes {blog.likes} <button>like</button>
+                    <br />
+                    {blog.author}
+                    <br />
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Blog;
