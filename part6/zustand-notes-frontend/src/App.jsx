388139a4d63ac9b550240ce1cd27728fd62869d6
Michal Dudek <michaldudek.contact@gmail.com>
2026-08-10 22:23:10 +0200

    Add addNot functionality

--- part6/zustand-notes-frontend/src/App.jsx
+++ part6/zustand-notes-frontend/src/App.jsx
@@ -1,10 +1,24 @@
-import { useNotes } from './store';
+import { useNotes, useNoteActions } from './store';
 
 const App = () => {
     const notes = useNotes();
+    const { add } = useNoteActions();
+
+    const generateId = () => Number((Math.random() * 1000000).toFixed(0));
+
+    const addNote = (e) => {
+        e.preventDefault();
+        const content = e.target.note.value;
+        add({ id: generateId(), content, important: false });
+        e.target.reset();
+    };
 
     return (
         <div>
+            <form onSubmit={addNote}>
+                <input name="note" />
+                <button type="submit">add</button>
+            </form>
             <ul>
                 {notes.map((note) => (
                     <li key={note.id}>
@@ -19,4 +33,5 @@ const App = () => {
         </div>
     );
 };
+
 export default App;
--- part6/zustand-notes-frontend/src/store.js
+++ part6/zustand-notes-frontend/src/store.js
@@ -1,13 +1,11 @@
 import { create } from 'zustand';
 
 const useNoteStore = create((set) => ({
-    notes: [
-        {
-            id: 1,
-            content: 'Zustand is less complex than Redux',
-            important: true,
-        },
-    ],
+    notes: [],
+    actions: {
+        add: (note) => set((state) => ({ notes: state.notes.concat(note) })),
+    },
 }));
 
 export const useNotes = () => useNoteStore((state) => state.notes);
+export const useNoteActions = () => useNoteStore((state) => state.actions);
