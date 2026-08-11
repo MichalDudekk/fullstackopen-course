ef1ba06d61800150dcaa4c7498ac9e63a3effa12
Michal Dudek <michaldudek.contact@gmail.com>
2026-08-11 13:24:09 +0200

    Add toggleImportance of notes (zustand-notes-app)

--- part6/zustand-notes-frontend/src/store.js
+++ part6/zustand-notes-frontend/src/store.js
@@ -4,6 +4,14 @@ const useNoteStore = create((set) => ({
     notes: [],
     actions: {
         add: (note) => set((state) => ({ notes: state.notes.concat(note) })),
+        toggleImportance: (noteId) =>
+            set((state) => ({
+                notes: state.notes.map((note) =>
+                    note.id === noteId
+                        ? { ...note, important: !note.important }
+                        : note,
+                ),
+            })),
     },
 }));
 
