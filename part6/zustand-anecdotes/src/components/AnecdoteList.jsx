3549f697666e81ea5ac04f96963cbe753fb10f72
Michal Dudek <michaldudek.contact@gmail.com>
2026-08-11 14:46:38 +0200

    Add sorting anecdotes by votes (Ad 6.5)

--- part6/zustand-anecdotes/src/components/AnecdoteList.jsx
+++ part6/zustand-anecdotes/src/components/AnecdoteList.jsx
@@ -6,15 +6,19 @@ const AnecdoteList = () => {
 
     return (
         <>
-            {anecdotes.map((anecdote) => (
-                <div key={anecdote.id}>
-                    <div>{anecdote.content}</div>
-                    <div>
-                        has {anecdote.votes}
-                        <button onClick={() => vote(anecdote.id)}>vote</button>
+            {anecdotes
+                .toSorted((a, b) => b.votes - a.votes)
+                .map((anecdote) => (
+                    <div key={anecdote.id}>
+                        <div>{anecdote.content}</div>
+                        <div>
+                            has {anecdote.votes}
+                            <button onClick={() => vote(anecdote.id)}>
+                                vote
+                            </button>
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
         </>
     );
 };
