7413443fd3e8866f51ad124971e4cfcd98f277ef
Michal Dudek <michaldudek.contact@gmail.com>
2026-08-16 02:27:13 +0200

    test: add test for voting (Ad 6.15)

--- part6/zustand-anecdotes/src/store.test.js
+++ part6/zustand-anecdotes/src/store.test.js
@@ -88,4 +88,38 @@ describe('useAnecdoteActions', () => {
         expect(anecdoteResult.current).toContain(mockAnegdotes[1]);
         expect(anecdoteResult.current).not.toContain(mockAnegdotes[0]);
     });
+
+    it('voting increases a number of votes of an anecdote', async () => {
+        const mockAnecdotes = [
+            {
+                content: 'If it hurts, do it more often',
+                id: '47145',
+                votes: 9,
+            },
+        ];
+        anecdoteService.getAll.mockResolvedValue(mockAnecdotes);
+        anecdoteService.update.mockResolvedValue({
+            ...mockAnecdotes[0],
+            votes: mockAnecdotes[0].votes + 1,
+        });
+
+        const { result } = renderHook(() => useAnecdoteActions());
+        await act(async () => {
+            await result.current.initialize();
+        });
+
+        await act(async () => await result.current.vote('47145'));
+        const { result: anecdoteResult1 } = renderHook(() => useAnecdotes());
+        expect(anecdoteResult1.current).toContainEqual({
+            ...mockAnecdotes[0],
+            votes: 10,
+        });
+
+        await act(async () => await result.current.vote('47145'));
+        const { result: anecdoteResult2 } = renderHook(() => useAnecdotes());
+        expect(anecdoteResult2.current).toContainEqual({
+            ...mockAnecdotes[0],
+            votes: 10,
+        });
+    });
 });
