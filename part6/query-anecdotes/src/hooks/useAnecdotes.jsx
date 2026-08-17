b26922464781ea51080915ba38ccf18d31a3a22e
Michal Dudek <michaldudek.contact@gmail.com>
2026-08-18 01:38:51 +0200

    feat: add createAnecdote method to useAnecdotes (Ad 6.17)

--- part6/query-anecdotes/src/hooks/useAnecdotes.jsx
+++ part6/query-anecdotes/src/hooks/useAnecdotes.jsx
@@ -1,16 +1,29 @@
-import { useQuery } from '@tanstack/react-query';
-import { getAnecdotes } from '../services/anegdotes';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { getAnecdotes, createAnecdote } from '../services/anegdotes';
 
 export const useAnecdotes = () => {
+    const client = useQueryClient();
+
     const result = useQuery({
         queryKey: ['anecdotes'],
         queryFn: getAnecdotes,
         retry: 1,
     });
 
+    const newAnecdoteMutation = useMutation({
+        mutationFn: createAnecdote,
+        onSuccess: (newAnecdote) => {
+            const anecdotes = client.getQueryData(['anecdotes']);
+            client.setQueryData(['anecdotes'], anecdotes.concat(newAnecdote));
+        },
+    });
+
     return {
         anecdotes: result.data,
         isPending: result.isPending,
         isError: result.isError,
+        createAnecdote: (content) => {
+            newAnecdoteMutation.mutate({ content, votes: 0 });
+        },
     };
 };
--- part6/query-anecdotes/src/services/anegdotes.js
+++ part6/query-anecdotes/src/services/anegdotes.js
@@ -2,5 +2,22 @@ const baseUrl = 'http://localhost:3001/anecdotes';
 
 export const getAnecdotes = async () => {
     const response = await fetch(baseUrl);
+    if (!response.ok) {
+        throw new Error('Failed to fetch notes');
+    }
+    return await response.json();
+};
+
+export const createAnecdote = async (anecdote) => {
+    const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'Application/json' },
+        body: JSON.stringify(anecdote),
+    };
+
+    const response = await fetch(baseUrl, options);
+    if (!response.ok) {
+        throw new Error('Failed to fetch notes');
+    }
     return await response.json();
 };
