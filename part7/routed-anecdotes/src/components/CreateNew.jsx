003d5604a9f805b58df4591d7c83dfe090786790
Michal Dudek <michaldudek.contact@gmail.com>
2026-08-22 22:55:21 +0200

    refactor: add useField for handling form (Ad. 7.1)

--- part7/routed-anecdotes/src/components/CreateNew.jsx
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -1,38 +1,43 @@
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+import { useField } from '../hooks';
 
 const CreateNew = ({ addAnecdote }) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
-  const navigate = useNavigate()
+    const content = useField('text');
+    const author = useField('text');
+    const info = useField('text');
+    const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    addAnecdote({ content, author, info, votes: 0 })
-    navigate('/')
-  }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addAnecdote({
+            content: content.value,
+            author: author.value,
+            info: info.value,
+            votes: 0,
+        });
+        navigate('/');
+    };
 
-  return (
-    <div>
-      <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
+    return (
         <div>
-          content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+            <h2>create a new anecdote</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    content
+                    <input {...content} />
+                </div>
+                <div>
+                    author
+                    <input {...author} />
+                </div>
+                <div>
+                    url for more info
+                    <input {...info} />
+                </div>
+                <button>create</button>
+            </form>
         </div>
-        <div>
-          author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
-        </div>
-        <div>
-          url for more info
-          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
-        </div>
-        <button>create</button>
-      </form>
-    </div>
-  )
-}
+    );
+};
 
-export default CreateNew
+export default CreateNew;
