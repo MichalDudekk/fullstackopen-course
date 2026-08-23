4736029ecfaa05dc12b33df7a8bda0dbefb90d10
Michal Dudek <michaldudek.contact@gmail.com>
2026-08-23 08:10:41 +0200

    fix: fix warning 'Invalid value for prop  on <input> tag' (Ad 7.3)

--- part7/routed-anecdotes/src/components/CreateNew.jsx
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -2,9 +2,9 @@ import { useNavigate } from 'react-router-dom';
 import { useField } from '../hooks';
 
 const CreateNew = ({ addAnecdote }) => {
-    const content = useField('text');
-    const author = useField('text');
-    const info = useField('text');
+    const { reset: resetContent, ...content } = useField('text');
+    const { reset: resetAuthor, ...author } = useField('text');
+    const { reset: resetInfo, ...info } = useField('text');
     const navigate = useNavigate();
 
     const handleSubmit = (e) => {
@@ -38,9 +38,9 @@ const CreateNew = ({ addAnecdote }) => {
                 <button
                     type="button"
                     onClick={() => {
-                        content.reset();
-                        author.reset();
-                        info.reset();
+                        resetContent();
+                        resetAuthor();
+                        resetInfo();
                     }}
                 >
                     reset
