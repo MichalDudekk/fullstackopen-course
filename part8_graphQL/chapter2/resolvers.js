acd142153cc37ed193c1cd2b85b31059f5b80333
Michal Dudek <michaldudek.contact@gmail.com>
2026-09-04 01:15:50 +0200

    feat: add User and user authentication

--- part8_graphQL/chapter2/resolvers.js
+++ part8_graphQL/chapter2/resolvers.js
@@ -1,5 +1,7 @@
 const { GraphQLError } = require('graphql');
+const jwt = require('jsonwebtoken');
 const Person = require('./models/person');
+const User = require('./models/user');
 
 let persons = [
     {
@@ -35,6 +37,9 @@ const resolvers = {
             return Person.find({ phone: { $exists: args.phone === 'YES' } });
         },
         findPerson: (root, args) => Person.findOne({ name: args.name }),
+        me: (root, args, context) => {
+            return context.currentUser;
+        },
     },
     Person: {
         address: ({ street, city }) => {
@@ -58,7 +63,23 @@ const resolvers = {
             }
 
             const person = new Person({ ...args });
-            return person.save();
+
+            try {
+                await person.save();
+            } catch (error) {
+                throw new GraphQLError(
+                    `Saving person failed: ${error.message}`,
+                    {
+                        extensions: {
+                            code: 'BAD_USER_INPUT',
+                            invalidArgs: args.name,
+                            error,
+                        },
+                    },
+                );
+            }
+
+            return person;
         },
         editNumber: async (root, args) => {
             const person = await Person.findOne({ name: args.name });
@@ -68,7 +89,57 @@ const resolvers = {
             }
 
             person.phone = args.phone;
-            return person.save();
+
+            try {
+                await person.save();
+            } catch (error) {
+                throw new GraphQLError(
+                    `Saving number failed: ${error.message}`,
+                    {
+                        extensions: {
+                            code: 'BAD_USER_INPUT',
+                            invalidArgs: args.name,
+                            error,
+                        },
+                    },
+                );
+            }
+
+            return person;
+        },
+        createUser: async (root, args) => {
+            const user = new User({ username: args.username });
+
+            return user.save().catch((error) => {
+                throw new GraphQLError(
+                    `Creating the user failed: ${error.message}`,
+                    {
+                        extensions: {
+                            code: 'BAD_USER_INPUT',
+                            invalidArgs: args.username,
+                            error,
+                        },
+                    },
+                );
+            });
+        },
+        login: async (root, args) => {
+            const user = await User.findOne({ username: args.username });
+
+            if (!user || args.password !== 'secret') {
+                throw new GraphQLError('wrong credentials', {
+                    extensions: {
+                        code: 'BAD_USER_INPUT',
+                    },
+                });
+            }
+
+            const userForToken = {
+                username: user.username,
+                id: user._id,
+            };
+
+            return { value: jwt.sign(userForToken, process.env.JWT_SECRET) };
         },
     },
 };
