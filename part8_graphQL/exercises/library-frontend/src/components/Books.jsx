290e3aa54c72682a1610cc0f356c4d8341fdc74e
Michal Dudek <michaldudek.contact@gmail.com>
2026-09-08 00:50:08 +0200

    feat: add genre filtering on frontend (Ad 8.20)

--- part8_graphQL/exercises/library-frontend/src/components/Books.jsx
+++ part8_graphQL/exercises/library-frontend/src/components/Books.jsx
@@ -1,8 +1,11 @@
+import { useState } from 'react';
+
 import { useQuery } from '@apollo/client/react';
 import { ALL_BOOKS } from '../queries';
 
 const Books = (props) => {
     const result = useQuery(ALL_BOOKS);
+    const [genreFilter, setGenreFilter] = useState(null);
 
     if (result.loading) {
         return null;
@@ -13,10 +16,18 @@ const Books = (props) => {
     }
 
     const books = result.data.allBooks;
+    const allGenres = books.map((book) => book.genres).flat();
+    const uniqueGenres = [...new Set(allGenres)];
 
     return (
         <div>
             <h2>books</h2>
+
+            {genreFilter && (
+                <p>
+                    in genre <b>{genreFilter}</b>
+                </p>
+            )}
 
             <table>
                 <tbody>
@@ -25,15 +36,28 @@ const Books = (props) => {
                         <th>author</th>
                         <th>published</th>
                     </tr>
-                    {books.map((a) => (
-                        <tr key={a.id}>
-                            <td>{a.title}</td>
-                            <td>{a.author.name}</td>
-                            <td>{a.published}</td>
-                        </tr>
-                    ))}
+                    {books
+                        .filter((book) =>
+                            genreFilter
+                                ? book.genres.includes(genreFilter)
+                                : true,
+                        )
+                        .map((a) => (
+                            <tr key={a.id}>
+                                <td>{a.title}</td>
+                                <td>{a.author.name}</td>
+                                <td>{a.published}</td>
+                            </tr>
+                        ))}
                 </tbody>
             </table>
+
+            {uniqueGenres.map((genre) => (
+                <button key={genre} onClick={() => setGenreFilter(genre)}>
+                    {genre}
+                </button>
+            ))}
+            <button onClick={() => setGenreFilter(null)}>all genres</button>
         </div>
     );
 };
